ada555d9bc3cdaec81db875adce946a9e093ad7d
Ivanproyectos <66085756+Ivanproyectos@users.noreply.github.com>
2022-02-20 21:20:59 -0500

    sdsf

---
+++ App_Ventas/App_Ventas/Scripts/General.js
@@ -0,0 +1,15 @@
+﻿/////////// para los menus active open; 
+$('.quixnav .metismenu ul a ').click(function () {
+    $('.quixnav .metismenu ul a ').removeClass('quixnav_a_active quixnav_a_active_before');
+    $(this).addClass('quixnav_a_active quixnav_a_active_before');
+    $(this).addClass('quixnav_a_active quixnav_a_active_before');
+
+    var _liPadre = $(this).parent().parent().parent();
+    var _aPadre = _liPadre.children();
+    var _TextPadre = _aPadre[0].innerText;
+    var _TextHijo = $(this).text(); 
+
+    $('#Text_padre').text(_TextPadre);
+    $('#Text_Hijo').text(_TextHijo);
+    //_liPadre.children();
+});
---
+++ App_Ventas/App_Ventas/Scripts/SweetAlert.js
@@ -0,0 +1,53 @@
+﻿(function ($) {
+    //"use strict"
+
+    /*******************
+    Sweet-alert JS
+    *******************/
+
+ 
+
+    jConfirm = function (message, title, callback) {
+        swal.fire({
+            title: title,
+            text: message,
+            icon: "warning",
+            showCancelButton: !0,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Aceptar",
+            cancelButtonText: "Cancelar",
+            closeOnConfirm: !1,
+            closeOnCancel: !1
+        }).then((result) => {
+            if (result.isConfirmed) {
+          callback(true); 
+        } else {
+            callback(false); 
+        }
+      }); 
+    }
+
+jOkas = function (message, title){
+    Swal.fire({
+        title: title,
+        text : message,
+        icon:'success',
+        confirmButtonText: 'Aceptar'
+    }
+  ); 
+}
+
+jError = function (message, title){
+    Swal.fire({
+        title: title,
+        text : message,
+        icon:'error',
+        confirmButtonText: 'Aceptar'
+    }
+  ); 
+}
+
+
+
+
+})(jQuery);
