729b51869f611db676c9ebcf8ef9af84fee4ffa0
Ivanproyectos <66085756+Ivanproyectos@users.noreply.github.com>
2022-02-26 21:42:18 -0500

    a

---
+++ App_Ventas/App_Ventas/Areas/Administracion/AdministracionAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace App_Ventas.Areas.Administracion
+{
+    public class AdministracionAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Administracion";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Administracion_default",
+                "Administracion/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
--- App_Ventas/App_Ventas/Scripts/General.js
+++ App_Ventas/App_Ventas/Scripts/General.js
@@ -1,5 +1,6 @@
 ﻿/////////// para los menus active open; 
 $('.quixnav .metismenu ul a ').click(function () {
+    var _this = $(this).parent().children(); 
     $('.quixnav .metismenu ul a ').removeClass('quixnav_a_active quixnav_a_active_before');
     $(this).addClass('quixnav_a_active quixnav_a_active_before');
     $(this).addClass('quixnav_a_active quixnav_a_active_before');
@@ -13,3 +14,15 @@ $('.quixnav .metismenu ul a ').click(function () {
     $('#Text_Hijo').text(_TextHijo);
     //_liPadre.children();
 });
+
+
+
+/////////// para los menus sin grupo; 
+$('.quixnav .metismenu li .not-before').click(function () {
+    var _this = $(this);
+    var _TextHijo = _this[0].innerText;
+    $('#Text_padre').text('Home');
+    $('#Text_Hijo').text(_TextHijo);
+
+});
+
