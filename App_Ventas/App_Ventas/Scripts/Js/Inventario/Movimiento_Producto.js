fff0bf00f0b885dcbf867b0a3fd8e616229cc665
Ivanproyectos <66085756+Ivanproyectos@users.noreply.github.com>
2022-04-29 23:15:45 -0500

    a

--- App_Ventas/App_Ventas/Scripts/Js/Inventario/Movimiento_Producto.js
+++ App_Ventas/App_Ventas/Scripts/Js/Inventario/Movimiento_Producto.js
@@ -1,15 +1,16 @@
 ﻿
 function Producto_Movimiento_Insertar(Tipo) {
-    var mensaje = ""; 
-    if (Tipo == 1)
-        mensaje = "<span class=\"text-success\">aumentar</span>";
-    else if (Tipo == 2)
-        mensaje = "<span class=\"text-danger\">disminuir</span>";
-    if ($("#frmMantenimiento_BuscarProducto").valid()) {
-        jConfirm("¿ Desea " + mensaje + " este producto en  <b>" + $("#CANTIDAD").val() + " " +
-            $("#HDF_COD_UNIDAD_MEDIDA").val() + "</b> al stock actual ?", "Atención", function (r) {
-                if (r) {
-                    var STOCK = $("#CANTIDAD").val();
+    if (!isNaN($("#CANTIDAD").val()) && $("#CANTIDAD").val() != 0) {
+        var mensaje = "";
+        if (Tipo == 1)
+            mensaje = "<span class=\"text-success\">aumentar</span>";
+        else if (Tipo == 2)
+            mensaje = "<span class=\"text-danger\">disminuir</span>";
+        if ($("#frmMantenimiento_BuscarProducto").valid()) {
+            jConfirm("¿ Desea " + mensaje + " este producto en  <b>" + $("#CANTIDAD").val() + " " +
+                $("#HDF_COD_UNIDAD_MEDIDA").val() + "</b> al stock actual ?", "Atención", function (r) {
+                    if (r) {
+                        var STOCK = $("#CANTIDAD").val();
                         if ($("#ID_UNIDAD_MEDIDA").val() == 1) // si es kilos guardo en gramos 
                         {
                             STOCK = ConvertKilos_Gramos(STOCK);
@@ -19,27 +20,30 @@ function Producto_Movimiento_Insertar(Tipo) {
                                 FLG_MOVIMIENTO: Tipo,
                                 CANTIDAD: STOCK,
                                 ID_PRODUCTO: $("#hfd_ID_PRODUCTO").val(),
-                                DETALLE:  $("#DETALLE").val(),
+                                DETALLE: $("#DETALLE").val(),
                                 USU_CREACION: $('#input_hdcodusuario').val(),
                                 ACCION: $("#AccionProducto").val()
                             };
-   
-                    var url = baseUrl + 'Inventario/Producto/Producto_Movimiento_Insertar';
-                    var auditoria = SICA.Ajax(url, item, false);
-                    if (auditoria != null && auditoria != "") {
-                        if (auditoria.EJECUCION_PROCEDIMIENTO) {
-                            if (!auditoria.RECHAZAR) {
-                                Producto_ConfigurarGrilla();
-                                Producto_Cerrar();
-                                jOkas("Proceso culminado satisfactoriamente.", "Proceso");
+
+                        var url = baseUrl + 'Inventario/Producto/Producto_Movimiento_Insertar';
+                        var auditoria = SICA.Ajax(url, item, false);
+                        if (auditoria != null && auditoria != "") {
+                            if (auditoria.EJECUCION_PROCEDIMIENTO) {
+                                if (!auditoria.RECHAZAR) {
+                                    Producto_ConfigurarGrilla();
+                                    Producto_Cerrar();
+                                    jOkas("Proceso culminado satisfactoriamente.", "Proceso");
+                                } else {
+                                    jError(auditoria.MENSAJE_SALIDA, "Atención");
+                                }
                             } else {
                                 jError(auditoria.MENSAJE_SALIDA, "Atención");
                             }
-                        } else {
-                            jError(auditoria.MENSAJE_SALIDA, "Atención");
                         }
                     }
-                }
-            });
+                });
         }
+    } else {
+        jError("La cantidad a ingresar debe ser mayor a cero.", "Atención");
+    }
 }
---
+++ App_Ventas/Capa_Entidad/Inventario/Cls_Ent_Translado_Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad.Inventario
+{
+   public class Cls_Ent_Translado_Producto : Base.Cls_Ent_Base
+    {
+       public int ID_TRANSLADO { get; set; }
+       public int ID_SUCURSAL_ORIGEN { get; set; }
+       public int ID_SUCURSAL_DESTINO { get; set; }
+       public string DETALLE { get; set; }
+       public List<Cls_Ent_Movimiento_Producto> Lista_Detalle { get; set; }
+    }
+}
