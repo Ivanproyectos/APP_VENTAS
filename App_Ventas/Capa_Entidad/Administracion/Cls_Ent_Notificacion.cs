3c6365c96a4c6a222cd70234443009e212231600
Ivanproyectos <66085756+Ivanproyectos@users.noreply.github.com>
2022-08-05 10:33:59 -0500

    a

---
+++ App_Ventas/Capa_Entidad/Administracion/Cls_Ent_Notificacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad.Administracion
+{
+    public class Cls_Ent_Notificacion
+    {
+
+    }
+}
